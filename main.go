16022b1c0cb019c1568374dc1251a05382137907
davidwilby <d.wilby@sheffield.ac.uk>
2019-10-29 17:15:37 +0000

    add logging to file (not working)

--- main.go
+++ main.go
@@ -8,11 +8,16 @@ package main
 // http://localhost:8800/api/v201910/?apikey=APIKEY&product=PRODUCT&uuid=UUID&eventID=EVENTID&dateTime=DATETIME
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var b io.Writer
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
@@ -22,11 +27,22 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 	version := r.FormValue("version")
 	eventID := r.FormValue("eventID")
 	dateTime := r.FormValue("dateTime")
-	fmt.Printf("apikey %s, product %s, version %s, uuid %s, eventID %s, dateTime %s, path %s\n",
+	fmt.Fprintf(b, "apikey %s, product %s, version %s, uuid %s, eventID %s, dateTime %s, path %s\n",
 		apikey, product, version, uuid, eventID, dateTime, path)
 }
 
 func main() {
+	// creates a new log file with a timestamped name
+	t := time.Now()
+	fname := "ingalog_" + t.Format("20060102150405") + ".log"
+	f, err := os.OpenFile(fname, os.O_CREATE|os.O_WRONLY, 0444)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	buf := bufio.NewWriter(f)
+	b = buf
+
 	http.HandleFunc("/api/v201910/", apiHandler)
 
 	port := os.Getenv("INGA_PORT")
@@ -36,4 +52,6 @@ func main() {
 	port = ":" + port
 	fmt.Fprintln(os.Stderr, "Listening on", port)
 	log.Fatal(http.ListenAndServe(port, nil))
+
+	buf.Flush()
 }
