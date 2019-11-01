7c5e5783d7bd7bbbafff8be0ba7636e307e547e3
davidwilby <d.wilby@sheffield.ac.uk>
2019-11-01 16:49:39 +0000

    add function to ping inga on startup

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 )
@@ -24,10 +25,30 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 	product := r.FormValue("product")
 	uuid := r.FormValue("uuid")
 	version := r.FormValue("version")
-	eventID := r.FormValue("eventID")
-	dateTime := r.FormValue("dateTime")
-	fmt.Fprintf(logFile, "apikey %s, product %s, version %s, uuid %s, eventID %s, dateTime %s, path %s\n",
+	eventID := r.FormValue("eventid")
+	dateTime := r.FormValue("datetime")
+	fmt.Fprintf(logFile, "%s, %s, %s, %s, %s, %s, %s\n",
 		apikey, product, version, uuid, eventID, dateTime, path)
+}
+
+func pinga() {
+	// pings the inga server when this instance of inga is started
+	target := "http://inga.shef.ac.uk:80/api/v201910/"
+	fmt.Fprintf(os.Stderr, "Pinging inga @ %s ...\n", target)
+
+	form := url.Values{}
+	form.Add("apikey", "ak-sample")
+	form.Add("product", "inga")
+	form.Add("uuid", "inga")
+	form.Add("version", "v201910")
+	form.Add("eventid", "inga")
+	t := time.Now()
+	form.Add("datetime", t.Format("20060102150405"))
+	resp, err := http.PostForm(target, form)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func main() {
@@ -40,6 +61,7 @@ func main() {
 	}
 	defer f.Close()
 	logFile = io.Writer(f)
+	fmt.Fprintf(logFile, "apikey, product, version, uuid, eventid, datetime, path\n") //print csv header
 
 	http.HandleFunc("/api/v201910/", apiHandler)
 
@@ -49,6 +71,6 @@ func main() {
 	}
 	port = ":" + port
 	fmt.Fprintln(os.Stderr, "Listening on", port)
+	go pinga()
 	log.Fatal(http.ListenAndServe(port, nil))
-
 }
